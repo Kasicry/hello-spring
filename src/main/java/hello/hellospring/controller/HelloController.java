7da04cbba45166ccc012d746257369e0a06bfddd
Geon <akrmak12@naver.com>
2021-12-08 23:35:27 +0900

    @ResponseBody API 방식 객체를 반환하는 방법 학습

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -14,9 +15,45 @@ public class HelloController {
             return "hello";
         }
 
-        @GetMapping("hello-mvc")
+        @GetMapping("hello-mvc") // 템플릿 방식
         public String helloMvc(@RequestParam(value = "name") String name, Model model){
             model.addAttribute("name", name);
             return "hello-template";
         }
-    }
+
+        @GetMapping("hello-string") // API 방식
+        @ResponseBody // 그대로 데이타를 내려주는 방식
+        public String helloString(@RequestParam("name") String name){
+            return "hello" + name; // hello spring
+        }
+
+        @GetMapping("hello-api")
+        @ResponseBody
+        public Hello helloApi(@RequestParam("name") String name){
+            Hello hello = new Hello(); // 객체로 Hello 선언
+            hello.setName(name);    // JSON 방식으로 출력 key는 name | JSON방식이 기본이 되었음
+            return hello;
+
+            // @ResponseBody를 사용 - API 방식 - Default JSON
+                // HTTP의 BODY에 문자 내용을 직접 반환
+                // viewResolver 대신 HttpMessageConverter가 동작
+                // 기본 문자처리 : StringHttpMessageConverter
+                // 기본 객체처리 : MappingJackson2HttpMessageConverter
+                // byte 처리 등등 기타 여러 HttpMessageConverter가 기본으로 등록되어 있음
+
+        }
+
+         static class Hello{
+            private String name; // 프로퍼티 접근방식? (게터 세터)
+
+             public String getName() {
+                 return name;
+             }
+
+             public void setName(String name) {
+                 this.name = name;
+             }
+         }
+         }
+
+
